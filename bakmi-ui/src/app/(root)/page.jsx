db3685dfb6215e6c131952c21a9dada3f16e17c6
Andry Ariadi <andryariadi23@gmail.com>
2024-01-12 21:38:03 +0700

    styling menulink component

--- bakmi-ui/src/app/(root)/page.jsx
+++ bakmi-ui/src/app/(root)/page.jsx
@@ -1,12 +1,14 @@
 import Level from "@/components/Level";
+import MenuLink from "@/components/MenuLink";
 import Point from "@/components/Point";
 
 export default function Home() {
   return (
     <>
-      <section className="wrapper flex flex-col gap-8">
+      <section className="wrapper flex flex-col gap-8 md:gap-10">
         <Level />
         <Point />
+        <MenuLink />
       </section>
     </>
   );
---
+++ bakmi-ui/src/components/MenuLink.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function MenuLink() {
+  return (
+    <>
+      <div className="flex flex-col md:flex-row gap-5 md:gap-8 w-full justify-evenly">
+        <div className="flex justify-center gap-10 md:gap-16">
+          <div className="flex flex-col items-center gap-3">
+            <Image src="/icons/order.png" width={30} height={30} alt="order" />
+            <p>Order</p>
+          </div>
+          <div className="flex flex-col items-center gap-3 ps-5">
+            <Image src="/icons/redeem.png" width={30} height={30} alt="redeem" />
+            <p>Redeem</p>
+          </div>
+          <div className="flex flex-col items-center gap-3">
+            <Image src="/icons/voucher.png" width={30} height={30} alt="myvoucher" />
+            <p>My Voucher</p>
+          </div>
+        </div>
+        <div className="flex justify-center gap-10 md:gap-16">
+          <div className="flex flex-col items-center gap-3">
+            <Image src="/icons/privilage.png" width={30} height={30} alt="privilage" />
+            <p>Privilage</p>
+          </div>
+          <div className="flex flex-col items-center gap-3 ps-5">
+            <Image src="/icons/calendar.png" width={30} height={30} alt="calender" />
+            <p>History</p>
+          </div>
+          <div className="flex flex-col items-center gap-3 pe-4">
+            <Image src="/icons/chat.png" width={30} height={30} alt="contact" />
+            <p>Contact Us</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
